a6ff05122d7f73cb9788d0bd3d86db3ecb035fbc
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-10-29 15:49:48 +0530

    ✅ Reactive form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ReactiveForm } from './components/reactive-form/reactive-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: ReactiveForm }];
---
+++ src/app/components/reactive-form/reactive-form.ts
@@ -0,0 +1,78 @@
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-reactive-form',
+  imports: [ReactiveFormsModule, AsyncPipe],
+  templateUrl: './reactive-form.html',
+  styleUrl: './reactive-form.scss',
+})
+export class ReactiveForm {
+  API = 'https://jsonplaceholder.typicode.com/posts';
+
+  userForm: FormGroup = new FormGroup({
+    id: new FormControl(0),
+    title: new FormControl(''),
+    body: new FormControl(''),
+  });
+
+  http = inject(HttpClient);
+  posts$: Observable<any[]>;
+
+  constructor() {
+    this.posts$ = this.http.get<any[]>(this.API);
+  }
+
+  handleSubmitFn() {
+    // debugger;
+    const userFormValue = this.userForm.value;
+
+    this.http.post(this.API, userFormValue).subscribe({
+      next: () => alert('User Is Registered'),
+      error: (e) => console.error(e),
+      complete: () => console.log('Complete'),
+    });
+  }
+
+  editFn(post: any) {
+    this.userForm = new FormGroup({
+      id: new FormControl(post.id),
+      title: new FormControl(post.title),
+      body: new FormControl(post.body),
+    });
+  }
+
+  updateFn() {
+    const userFormValue = this.userForm.value;
+    const url = `${this.API}/${userFormValue.id}`;
+
+    this.http.put(url, userFormValue).subscribe({
+      next: () => alert('User updated successfully!'),
+      error: (e) => console.error(e),
+      complete: () => {
+        this.userForm = new FormGroup({
+          id: new FormControl(0),
+          title: new FormControl(''),
+          body: new FormControl(''),
+        });
+        console.log('User Updated');
+      },
+    });
+  }
+
+  deleteFn(id: any) {
+    const url = `${this.API}/${id}`;
+    const isDelete = confirm('Are you sure you want to delete?');
+
+    if (isDelete) {
+      this.http.delete(url).subscribe({
+        next: () => alert('User is deleted'),
+        error: (e) => console.error(e),
+        complete: () => console.warn('User is deleted'),
+      });
+    }
+  }
+}

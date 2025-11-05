4cc208cae04e43e3ea4cf333ef13aaaa84d6dfca
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-11-05 12:32:57 +0530

    ✅ JSON Server/Type Class

--- src/app/components/reactive-form/reactive-form.ts
+++ src/app/components/reactive-form/reactive-form.ts
@@ -1,8 +1,22 @@
 import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+
+class NewUser {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.email = '';
+    this.password = '';
+  }
+}
 
 @Component({
   selector: 'app-reactive-form',
@@ -11,59 +25,97 @@ import { Observable } from 'rxjs';
   styleUrl: './reactive-form.scss',
 })
 export class ReactiveForm {
-  API = 'https://jsonplaceholder.typicode.com/posts';
+  userFormGrp: FormGroup<{
+    id: FormControl<number>;
+    name: FormControl<string>;
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }>;
 
-  formGroupForUser: FormGroup = new FormGroup({
-    id: new FormControl(0),
-    title: new FormControl(''),
-    body: new FormControl(''),
-  });
-
+  API = 'http://localhost:3000/users';
   http = inject(HttpClient);
-  posts$: Observable<any[]>;
+  users$: Observable<NewUser[]>;
 
   constructor() {
-    this.posts$ = this.http.get<any[]>(this.API);
-  }
+    const user = new NewUser();
 
-  handleSubmitFn() {
-    // debugger;
-    const userFormValue = this.formGroupForUser.value;
-
-    this.http.post(this.API, userFormValue).subscribe({
-      next: () => alert('User Is Registered'),
-      error: (e) => console.error(e),
-      complete: () => console.log('Complete'),
+    this.userFormGrp = new FormGroup({
+      id: new FormControl(user.id, { nonNullable: true }),
+      name: new FormControl(user.name, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+      email: new FormControl(user.email, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl(user.password, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     });
+
+    this.users$ = this.http.get<NewUser[]>(this.API);
   }
 
-  editFn(post: any) {
-    this.formGroupForUser = new FormGroup({
-      id: new FormControl(post.id),
-      title: new FormControl(post.title),
-      body: new FormControl(post.body),
+  createUserFn() {
+    if (this.userFormGrp.valid) {
+      // debugger;
+      const userFormGrpVal = this.userFormGrp.value;
+      // debugger;
+      this.http.post(this.API, userFormGrpVal).subscribe({
+        next: () => alert('User Is Registered'),
+        error: (e) => console.error(e),
+        complete: () => {
+          this.users$ = this.http.get<NewUser[]>(this.API);
+          console.log(userFormGrpVal);
+          console.log('Complete');
+          this.userFormGrp.reset();
+        },
+      });
+    }
+  }
+
+  editFn(user: NewUser) {
+    this.userFormGrp = new FormGroup({
+      id: new FormControl(user.id, { nonNullable: true }),
+      name: new FormControl(user.name, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+      email: new FormControl(user.email, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl(user.password, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     });
   }
 
   updateFn() {
-    const userFormValue = this.formGroupForUser.value;
-    const url = `${this.API}/${userFormValue.id}`;
+    const userFormGrpVal = this.userFormGrp.value;
+    const url = `${this.API}/${userFormGrpVal.id}`;
+    console.log(url);
 
-    this.http.put(url, userFormValue).subscribe({
+    this.http.put(url, userFormGrpVal).subscribe({
       next: () => alert('User updated successfully!'),
       error: (e) => console.error(e),
       complete: () => {
-        this.formGroupForUser = new FormGroup({
-          id: new FormControl(0),
-          title: new FormControl(''),
-          body: new FormControl(''),
+        this.users$ = this.http.get<NewUser[]>(this.API);
+        this.userFormGrp = new FormGroup({
+          id: new FormControl(0, { nonNullable: true }),
+          name: new FormControl('', { nonNullable: true }),
+          email: new FormControl('', { nonNullable: true }),
+          password: new FormControl('', { nonNullable: true }),
         });
         console.log('User Updated');
       },
     });
   }
 
-  deleteFn(id: any) {
+  deleteFn(id: number) {
     const url = `${this.API}/${id}`;
     const isDelete = confirm('Are you sure you want to delete?');
 
@@ -71,7 +123,10 @@ export class ReactiveForm {
       this.http.delete(url).subscribe({
         next: () => alert('User is deleted'),
         error: (e) => console.error(e),
-        complete: () => console.warn('User is deleted'),
+        complete: () => {
+          this.users$ = this.http.get<NewUser[]>(this.API);
+          console.warn('User is deleted');
+        },
       });
     }
   }
